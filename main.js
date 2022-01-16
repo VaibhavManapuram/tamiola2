ba14e26cfc43cd9320fc2e7a9cd226b64991bacf
vaibhav <vaibhavcreo@gmail.com>
2022-01-17 03:28:29 +0530

    initial

---
+++ main.js
@@ -0,0 +1,12 @@
+$(window).scroll( function(){
+    $('section').each( function(i){
+
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if( bottom_of_window > bottom_of_element ){
+            $(this).animate({'opacity':'1'},1000);
+        }
+
+    });
+});
